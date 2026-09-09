66b3b2c86d04e8d9d5b3dbabd0ad377f1c98d5e5
RubenPr <dcanayg@miumg.edu.gt>
2026-09-08 21:34:18 -0600

    Refactor code structure for improved readability and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'constants/app_strings.dart';
 import 'constants/app_theme.dart';
 import 'router/app_router.dart';
@@ -8,6 +11,10 @@ import 'services/database_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
 
   // Inicializar DB antes de runApp
   await DatabaseHelper.instance.database;
